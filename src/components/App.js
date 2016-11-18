a568f41589d664061a2f96c96406ffd8bf7a0fef
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-11-18 17:05:34 -0500

    working code mirror

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Codemirror from 'react-codemirror'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/mode/javascript/javascript.js'
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {code: ''}
+  }
+
+  updateCode(newCode) {
+    debugger;
+    this.setState({
+        code: newCode
+    });
+  }
+
   render() {
-    return (
-      <h1>hello sophie</h1>
-    )
+    var options = {
+        lineNumbers: true,
+        mode: 'javascript'
+    };
+    return <Codemirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
   }
 }
